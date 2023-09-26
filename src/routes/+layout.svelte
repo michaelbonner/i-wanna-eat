545330ec03afa133aa211314197326f8a3e91597
Michael Bonner <michaelwbonner@gmail.com>
2023-09-26 11:59:00 -0600

    add tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="p-8">
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl lg:text-5xl font-bold">I Wanna Eat</h1>
+<p>Where tho?</p>
